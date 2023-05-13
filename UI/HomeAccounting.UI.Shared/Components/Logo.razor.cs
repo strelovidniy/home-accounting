1f4f320c0dd3422a2ae1b1177897f7c15d2067ff
Roman Danylevych <rdanylevych@exoft.net>
2023-05-13 21:32:13 +0300

    Added logo

---
+++ UI/HomeAccounting.UI.Shared/Components/Logo.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HomeAccounting.UI.Shared.Components;
+
+public partial class Logo
+{
+    [Parameter]
+    public string? Class { get; set; } = string.Empty;
+}
