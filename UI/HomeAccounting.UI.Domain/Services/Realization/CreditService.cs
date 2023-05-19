e94af905d142ff6579744379c3370139619d985b
Nazariy Chetvertukha <nazariy.chetvertukha@gmail.com>
2023-05-19 14:50:18 +0300

    Credits dialog logic

---
+++ UI/HomeAccounting.UI.Domain/Services/Realization/CreditService.cs
@@ -0,0 +1,38 @@
+﻿using HomeAccounting.Models.Create;
+using HomeAccounting.Models.Update;
+using HomeAccounting.UI.Domain.Http.HomeAccountingHttpClient;
+using HomeAccounting.UI.Domain.Services.Abstraction;
+
+namespace HomeAccounting.UI.Domain.Services.Realization;
+
+internal class CreditService : ICreditService
+{
+    private readonly IHomeAccountingHttpClient _httpClient;
+
+    public CreditService(
+        IHomeAccountingHttpClient httpClient
+    ) => _httpClient = httpClient;
+
+    public Task CreateCreditAsync(CreateCreditModel model, CancellationToken cancellationToken = default)
+    => _httpClient
+        .PostAsync(
+            "api/v1/credits",
+            _httpClient.CreateJsonContent(model),
+            cancellationToken
+        );
+
+    public Task UpdateCreditAsync(UpdateCreditModel model, CancellationToken cancellationToken = default)
+    =>_httpClient
+        .PutAsync(
+            "api/v1/credits",
+            _httpClient.CreateJsonContent(model),
+            cancellationToken
+        );
+
+    public Task DeleteCreditAsync(Guid creditId, CancellationToken cancellationToken = default)
+        => _httpClient
+            .DeleteAsync(
+                $"api/v1/credits?creditId={creditId}",
+                cancellationToken
+            );
+}
